6d1750434ca82c907d0e82e15b6b514d6bc75e19
kaushalSTC <kaushal.kumar@seventhtriangle.com>
2025-09-29 16:23:20 +0530

    button replacement

--- src/components/TournamentLive/TournamentLiveData/TournamentLiveData.jsx
+++ src/components/TournamentLive/TournamentLiveData/TournamentLiveData.jsx
@@ -11,7 +11,7 @@ const TournamentLiveData = ({ Fixtures, Schedule, EventFormat }) => {
       <div className='w-full flex items-center justify-between border-b border-b-1c4ba3 bg-white px-9 md:px-20'>
         {TournamentLiveTabs.map((tab) => {
           return (
-            <button key={tab} className={`${selectedTab === tab ? 'border-b-1c4ba3 border-b-2' : '' } font-general font-medium text-sm md:text-base text-383838`} onClick={() => setSelectedTab(tab)}>{tab}</button>
+            <input type='button' key={tab}  value={tab} className={`${selectedTab === tab ? 'border-b-1c4ba3 border-b-2' : '' } font-general font-medium text-sm md:text-base text-383838`} onClick={() => setSelectedTab(tab)}/>
           )
         })}
       </div>
