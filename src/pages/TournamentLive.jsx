4be131052d6f9ffab82b9d605368372aeb851dea
kaushalSTC <kaushal.kumar@seventhtriangle.com>
2025-09-29 17:45:02 +0530

    vh update

--- src/pages/TournamentLive.jsx
+++ src/pages/TournamentLive.jsx
@@ -48,7 +48,7 @@ const TournamentLive = () => {
       return (
         <div
           key={index}
-          className={applyMargin ? "mb-[5vh]" : ""}
+          className={applyMargin ? "mb-[8vh]" : ""}
         >
           <item.Component tournament={tournamentData?.tournament} />
         </div>
